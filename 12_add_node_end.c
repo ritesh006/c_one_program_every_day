0e108d89122f80f7765fb187356322a1b6c0ea62
ritesh006 <ritesh.kumar0793@gmail.com>
2024-06-25 22:19:37 +0530

    12_add_node_end.c rewrite again

--- 12_add_node_end.c
+++ 12_add_node_end.c
@@ -1,50 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct record
-{
-    int data;
-    struct record *next;
-}sll;
+    #include <stdio.h>
+    #include <stdlib.h>
 
-void add_end(sll **head, int data)
-{
-    sll *p = *head;
-    sll *new_data = malloc(sizeof(sll));
+    typedef struct records{
+        int data;
+        struct records *next;
+    }sll;
 
-    new_data->data = data;
-    new_data->next = NULL;
-    if (*head == NULL) {
-        *head = new_data;  // If head is NULL, make new_data the head
-        return;
+
+    void add_end(sll **head, int data){
+        sll *new_data = malloc(sizeof(sll));
+        if(new_data == NULL)
+        {
+            printf("NO Memory allocated\n");
+            exit(1);
+        }
+        else{
+            printf("Memory Allocated!\n");
+        }
+        
+        new_data->data = data;
+        new_data->next = NULL;
+
+        sll *p = *head;
+
+        if(*head == NULL)
+        {
+            *head = new_data;
+            return;
+        }
+        while(p->next != NULL)
+        {
+            p = p->next;
+        }
+
+        p->next = new_data;
     }
 
-    while(p->next != NULL)
-    {
-        p = p->next;
+    void print(sll *head){
+        sll *temp = head;
+        if(head == NULL)
+        {
+            printf("NO data found\n");
+            exit(1);
+        }
+
+        while(temp != NULL)
+        {
+            printf("%d\n",temp->data);
+            temp = temp->next;
+        }
+
+
     }
 
-    p->next = new_data;
 
+    int main(){
+        sll *head = NULL;
 
-
-}
-int main()
-{
-    sll *head = NULL;
-
-    add_end(&head,10);
-add_end(&head, 20);
-    add_end(&head, 30);
-
-    // Print the linked list to verify
-    sll *current = head;
-    while (current != NULL) {
-        printf("%d -> ", current->data);
-        current = current->next;
+        add_end(&head, 10);
+        add_end(&head, 20);
+        add_end(&head, 30);
+        print(head);
+        return 0;
     }
-    printf("NULL\n");
-
-
-
-    return 0;
-}
